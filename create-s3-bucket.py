7fbb2a23ed869e501e74f44473444068f0016b24
submah <mahapatra.subratkumar@gmail.com>
2020-04-12 14:20:58 +0530

    Update create-s3-bucket.py

--- create-s3-bucket.py
+++ create-s3-bucket.py
@@ -1,9 +1,9 @@
 import boto3
-region = 'us-east-1'
-bucket_name = 'mydemobucket9090'
+region = 'eu-west-1'
+bucket_name = 'mydemobucket909090'
 #Create a sessiion
-session = boto3.session.Session(aws_access_key_id='PROVICE_YOUR_ACCESS_KEY',
-                     aws_secret_access_key='PROVICE_YOUR_SECRET_ACCESS_KEY', region_name=region)
+session = boto3.session.Session(aws_access_key_id='PROVIDE-YOUR-ACCESSKEY',
+                     aws_secret_access_key='PROVIDE-YOUR-SECRET-ACCESSKEY')
 
 #create s3 resource and client
 s3_res = session.resource(service_name='s3')
@@ -11,5 +11,11 @@ s3_cli = session.client(service_name='s3')
 
 
 # Create bucket
-bucket_response = s3_cli.create_bucket(Bucket=bucket_name)
+bucket_response = s3_cli.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={
+        'LocationConstraint': region})
 print(bucket_response)
+
+#Print all s3 bucket
+#for bucket in s3_res.buckets.all():
+#    print(bucket.name)
+
